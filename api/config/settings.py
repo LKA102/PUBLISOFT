017f5ccecc1d90d86d7897cf445b3c4bf7890708
LKA102 <stefano.hernandez@unmsm.edu.pe>
2025-05-11 20:50:38 -0500

    Modify JWT environment variable

--- api/config/settings.py
+++ api/config/settings.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     SUPABASE_URL: str
     SUPABASE_KEY: str
+    JWT_SECRET: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
     
